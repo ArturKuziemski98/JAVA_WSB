bd8d8ae0271e08ccde6a1545ae21db584d7ebc7b
arturkuziemski98 <kuziemskiartur98@gmail.com>
2021-03-14 23:27:23 +0100

    Zadanie 1 v3

---
+++ src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Car {
+    final String model;
+    final String producent;
+    public String color;
+
+Car(String model,String producent){
+    this.model = model;
+    this.producent = producent;
+}
+
+}
